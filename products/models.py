115a162f0b6879ac2db310d83b6ec32a6cbb0e2b
MeetAntala45 <meetantala45@gmail.com>
2024-03-13 19:40:41 +0530

    Category model and app created

--- products/models.py
+++ products/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+from category.models import Category  # Import the Category model from the categories app
 
 # Create your models here.
+
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     image = models.ImageField(upload_to='product_images/', blank=True, null=True)   # for image python -m pip install Pillow
 
     def __str__(self):
         return self.name
+
+
