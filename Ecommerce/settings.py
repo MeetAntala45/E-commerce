e89dbcbeef15582711cedc74a51fc09f58c12a12
MeetAntala45 <meetantala45@gmail.com>
2024-03-13 18:50:12 +0530

    Product model created

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -31,6 +31,10 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -41,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'products',
 ]
 
 MIDDLEWARE = [
